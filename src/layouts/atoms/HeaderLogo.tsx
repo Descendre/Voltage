42630765cf9932186927be2e7758d11999f8c123
Hinata0607 <atahin0607@gmail.com>
2024-07-06 19:58:39 +0900

    add: header logo

---
+++ src/layouts/atoms/HeaderLogo.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+
+export const HeaderLogo = () => {
+	return (
+		<Avatar
+			src="/SOZO_logo_transparent2.svg"
+			variant="square"
+			sx={{
+				width: '150px',
+			}}
+		/>
+	);
+};
