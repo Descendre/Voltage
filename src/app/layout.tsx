7758d05437dcf7435753c2bd02e0779e686e64e3
Hinata0607 <atahin0607@gmail.com>
2024-07-06 19:05:55 +0900

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "test01",
+  description: "test01",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
